febdf624d847da18cef7261f27473a9e7b533d7a
BaoLopTruong <ngocbaole1999@gmail.com>
2020-11-03 10:22:25 +0700

    ok

--- demo.php
+++ demo.php
@@ -1,8 +1,9 @@
 hello tam
 <<<<<<< HEAD
 vai ca noi tam
-=======
+
 fuck u. tam
 bye tam
-           ok tam
->>>>>>> 8025c8118c2a2a0dcb89779295e274992163b565
+vai ca noi tam 
+chot mo di tam 
+met qua tam oi 
